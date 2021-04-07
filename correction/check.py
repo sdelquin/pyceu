60b1a0a9acd970c530d654a061c67a9dd8ba3f4d
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-04-07 14:10:35 +0100

    Retrieve tasks from a given folder

--- correction/check.py
+++ correction/check.py
@@ -1,4 +1,3 @@
-import os
 import re
 import subprocess
 import sys
@@ -13,7 +12,6 @@ CORRECTION_DISPLAY = (
     ('red', '❌', 'NO APTO', '🙁'),
     ('green', '✅', 'APTO', '🥳'),
 )
-TASK_FILENAME = 'task.py'
 
 
 def parse_exception(exception_message):
@@ -24,7 +22,7 @@ def parse_exception(exception_message):
 
 
 def run_test(filename, args, desired_output):
-    command = f'python {filename} {args}'
+    command = f'python "{filename}" {args}'
     console.print(f'[dark_goldenrod]$ {command}')
     try:
         output = subprocess.check_output(
@@ -61,8 +59,7 @@ def inject_checking_code(code, input_vars, output_vars):
     return code
 
 
-def create_injected_asgmt_file(asgmt_filename, config):
-    asgmt_file = Path(asgmt_filename)
+def create_injected_asgmt_file(asgmt_file: Path, config: list):
     asgmt_code = asgmt_file.read_text()
     injected_asgmt_code = inject_checking_code(
         asgmt_code, config['vars']['input'], config['vars']['output']
@@ -73,10 +70,10 @@ def create_injected_asgmt_file(asgmt_filename, config):
     return injected_asgmt_file
 
 
-def handle_assignment(asgmt_id, asgmt_filename):
+def handle_assignment(asgmt_id: str, asgmt_file: Path):
     passed = []
     config = yaml.load(Path('config.yml').read_text(), Loader=yaml.FullLoader)[asgmt_id]
-    injected_asgmt_file = create_injected_asgmt_file(asgmt_filename, config)
+    injected_asgmt_file = create_injected_asgmt_file(asgmt_file, config)
 
     for case in config['cases']:
         args = ' '.join(str(v) for v in case['input'])
@@ -97,13 +94,23 @@ def handle_assignment(asgmt_id, asgmt_filename):
 
 
 if __name__ == '__main__':
+    """
+    sys.argv[1]: task identifier as written in config.yml
+    sys.argv[2]: path to the folder where tasks are saved (e.g. ~/Downloads)
+    """
     task_id = sys.argv[1]
-    task_path = sys.argv[2]
-    task_file = Path(task_path)
+    task_folder = Path(sys.argv[2])
+    try:
+        # It expects only one .py file to be checked
+        task_file = next(task_folder.glob('*.py'))
+    except StopIteration:
+        console.print(f'[dark_orange]No .py files found in {task_folder}')
+        sys.exit()
 
-    if not handle_assignment(task_id, task_file.absolute()):
+    console.print(f'[white bold]Checking "{task_file.name}"\n')
+    if not handle_assignment(task_id, task_file):
         filename = task_file.name
         sep = '-' * len(filename)
         console.print(f'[dark_goldenrod]\n{filename}\n{sep}')
         console.print(task_file.read_text())
-    os.remove(task_file)
+    task_file.unlink()
